3814ec1c71a4d191498d3248e0dd62aed267426b
saintrob <robertusnugroho03@gmail.com>
2026-07-22 21:56:39 -0400

    Fix fetch URL from 127.0.0.0 to 127.0.0.1

--- static/script.js
+++ static/script.js
@@ -1,11 +1,11 @@
 async function GetPlaylist() {
     try {
-        const response = await fetch('http://127.0.0.0:8000/getallplaylists');
+        const response = await fetch('http://127.0.0.1:8000/getallplaylists');
         if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        const data = response.json();
-        console.log(data);
+        const PlaylistData = await response.json();
+        console.log(PlaylistData);
     }
     catch (error) {
         console.error("Failed to fetch data:", error);

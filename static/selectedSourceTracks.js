626a3c79568c13564931a9ce510999cd9e893149
Robertus Nugroho <robertusnugroho03@gmail.com>
2026-08-27 17:37:23 -0400

    commit1

--- static/selectedSourceTracks.js
+++ static/selectedSourceTracks.js
@@ -3,21 +3,21 @@ async function selectSourceTracks() {
         const button = document.querySelector("#selectSourceTracks_button")
         const confirmButton = document.getElementById('selectSourceTracks_button');
 
-confirmButton.addEventListener('click', () => {
-  // 1. Select all checkboxes with name="fruit" that are currently checked (:checked)
-  const checkedBoxes = document.querySelectorAll('input[name="source_tracks"]:checked');
-  
-  // 2. Convert the NodeList into a standard array and map out just the values
-  const selectedValues = Array.from(checkedBoxes).map(cb => cb.value);
-  
-  // 3. Use the data
-  console.log("Selected Checkboxes:", selectedValues); 
+        confirmButton.addEventListener('click', () => {
+        // 1. Select all checkboxes with name="fruit" that are currently checked (:checked)
+        const checkedBoxes = document.querySelectorAll('input[name="source_tracks"]:checked');
         
-    }
-)}
-    catch (error) {
-        console.error("Failed to fetch data:", error);
-    }
-}
+        // 2. Convert the NodeList into a standard array and map out just the values
+        const selectedValues = Array.from(checkedBoxes).map(cb => cb.value);
+        
+        // 3. Use the data
+        console.log("Selected Checkboxes:", selectedValues); 
+                
+            }
+        )}
+            catch (error) {
+                console.error("Failed to fetch data:", error);
+            }
+        }
 
-selectSourceTracks();  
+selectSourceTracks();

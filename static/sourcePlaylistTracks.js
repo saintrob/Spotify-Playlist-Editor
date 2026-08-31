0121ea45f462eb34e1206cb4271f05365e8d2da6
Robertus Nugroho <robertusnugroho03@gmail.com>
2026-08-31 11:49:18 -0400

    added instructions on HTML

--- static/sourcePlaylistTracks.js
+++ static/sourcePlaylistTracks.js
@@ -6,11 +6,12 @@ async function sourcePlaylist_tracks() {
         button.addEventListener('click', async() => {
             try {
         const container = document.getElementById('radio-select-song-source');
+        
         const response = await fetch('/api/getsourceid/alltracks');
         if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        const source_track_data = await response.json();
+        
         
         for (let i = 0; i < source_track_data.length; i++) {
             const input = document.createElement("input");
